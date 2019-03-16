11521d19caf2ee5dece8f9a8705027d18167d094
steliostss <tsagarakis.stelios@gmail.com>
2019-03-16 20:54:52 +0200

    Add delta

--- delta.c
+++ delta.c
@@ -7,18 +7,17 @@ typedef struct {
 } tTuple;
 
 int students = 0;
-int COLUMNS; 
-int ROWS;
+
 int total_sensors = 0;
 tTuple final_sensors[1000];
 
-tTuple find_max(int** heat_map){
+tTuple find_max(int cols, int rows, int** heat_map){
     tTuple maxTuple;
     maxTuple.x = 0;
     maxTuple.y = 0;
     int max = 0;
-    for (int i=0; i<ROWS; ++i){
-        for (int j=0; j<COLUMNS; ++j){
+    for (int i=0; i<rows; ++i){
+        for (int j=0; j<cols; ++j){
             if (heat_map[i][j] > max) {
               maxTuple.x = i;
               maxTuple.y = j;  
@@ -28,7 +27,7 @@ tTuple find_max(int** heat_map){
     return maxTuple;
 }
 
-void edit_neighbors_on_heat_map(int col, int row, int radius, int** heat_map) {
+void edit_neighbors_on_heat_map(int cols, int rows, int col, int row, int radius, int** heat_map) {
     //set the borders of the "subgrid" to decrease value on heatmap
     //rows are set here
     int row_from, row_to;    
@@ -39,8 +38,8 @@ void edit_neighbors_on_heat_map(int col, int row, int radius, int** heat_map) {
 
     //stop row is set here
     //if we are too close to the right edge, start from the edge
-    if (row > COLUMNS-radius)
-        row_to = COLUMNS-1;
+    if (row > cols-radius)
+        row_to = cols-1;
     else 
         row_to = row+radius;
 
@@ -56,10 +55,11 @@ void edit_neighbors_on_heat_map(int col, int row, int radius, int** heat_map) {
 
     //stop column is set here
     //if we are too close to the right edge, start from the edge
-    if (col > ROWS-radius)
-        col_to = COLUMNS-1;
+    if (col > rows-radius)
+        col_to = cols-1;
     else 
         col_to = col+radius;
+
     for (int i=row_from; i<row_to; ++i) {
         for(int j=col_from; j<col_to; ++j){
             --heat_map[i][j];
@@ -67,14 +67,14 @@ void edit_neighbors_on_heat_map(int col, int row, int radius, int** heat_map) {
     }
 }
 
-void calculate_sensors(int col, int row, int radius, int** heat_map, int** student_seats) {
+void calculate_sensors(int cols, int rows, int col, int row, int radius, int** heat_map, int** student_seats) {
     int row_from, row_to;    
     if (row < radius)
         row_from = 0;
     else 
         row_from = row-radius;
-    if (row > COLUMNS-radius)
-        row_to = COLUMNS-1;
+    if (row > cols-radius)
+        row_to = cols-1;
     else 
         row_to = row+radius;
 
@@ -83,22 +83,23 @@ void calculate_sensors(int col, int row, int radius, int** heat_map, int** stude
         col_from = 0;
     else 
         col_from = col-radius;
-    if (col > ROWS-radius)
-        col_to = COLUMNS-1;
+    if (col > rows-radius)
+        col_to = cols-1;
     else 
         col_to = col+radius;
 
     for (int i=row_from; i<row_to; ++i) {
         for(int j=col_from; j<col_to; ++j){
             if (student_seats[i][j] == 1){
-                edit_neighbors_on_heat_map(i, j, radius, heat_map);
+                edit_neighbors_on_heat_map(cols, rows, i, j, radius, heat_map);
+                --students;
             }
         }
     }
 }
 
-void create_heat_map(int radius, int** heat_map, int** student_seats) {
-    for (int row=0; row<COLUMNS; ++row){
+void create_heat_map(int cols, int rows, int radius, int** heat_map, int** student_seats) {
+    for (int row=0; row<cols; ++row){
 
         int row_from;
         int row_to;
@@ -107,12 +108,12 @@ void create_heat_map(int radius, int** heat_map, int** student_seats) {
             row_from = 0;
         else 
             row_from = row-radius;
-        if (row > COLUMNS-radius)
-            row_to = COLUMNS-1;
+        if (row > cols-radius)
+            row_to = cols-1;
         else 
             row_to = row+radius;
 
-        for (int col=0; col<ROWS; ++col){
+        for (int col=0; col<rows; ++col){
             if (student_seats[row][col] == 0) 
                 continue;
             int col_from;
@@ -121,8 +122,8 @@ void create_heat_map(int radius, int** heat_map, int** student_seats) {
                 col_from = 0;
             else 
                 col_from = col-radius;
-            if (col > ROWS-radius)
-                col_to = COLUMNS-1;
+            if (col > rows-radius)
+                col_to = cols-1;
             else 
                 col_to = col+radius;
             for (int i=row_from; i<row_to; ++i){
@@ -136,6 +137,8 @@ void create_heat_map(int radius, int** heat_map, int** student_seats) {
 
 int main (int argc, char** argv) {
 
+    int COLUMNS; 
+    int ROWS;
     //Enter COLUMNS of the room:
     scanf("%d", &COLUMNS);
     
@@ -167,24 +170,6 @@ int main (int argc, char** argv) {
         }
     }
 
-    //Enter radius of sensors:;
-    int radius;
-    scanf("%d", &radius);
-
-    //DO THINGS HERE----------------------------------------------------
-    create_heat_map(radius, heat_map, student_seats);
-    while(students > 0) {
-        tTuple max = find_max(heat_map);
-        //add new sensor
-        final_sensors[total_sensors] = max;
-        //increase number of sensors
-        ++total_sensors;
-        //calculate next sensor
-        calculate_sensors(max.x, max.y, radius, heat_map, student_seats);
-    }
-
-    //------------------------------------------------------------------
-
     for (int i=0; i<COLUMNS; ++i) {
         for (int j=0; j<ROWS; ++j) {
             printf("%d ", student_seats[i][j]);
@@ -192,10 +177,57 @@ int main (int argc, char** argv) {
         printf("\n");
     }
 
+        printf("\n");
+        printf("\n");
+
+
+    //Enter radius of sensors:;
+    int radius;
+    scanf("%d", &radius);
+
+
+
+    //DO THINGS HERE----------------------------------------------------
+    create_heat_map(COLUMNS, ROWS, radius, heat_map, student_seats);
+
     printf("\n\n");   
 
     for (int i=0; i<COLUMNS; ++i) {
         for (int j=0; j<ROWS; ++j) {
+            printf("%d ", heat_map[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n"); 
+
+    while(students > 0) {
+        tTuple max = find_max(COLUMNS, ROWS, heat_map);
+        //add new sensor
+        final_sensors[total_sensors] = max;
+        //increase number of sensors
+        ++total_sensors;
+        //calculate next sensor
+        calculate_sensors(COLUMNS, ROWS, max.x, max.y, radius, heat_map, student_seats);
+    }
+
+    //------------------------------------------------------------------
+
+    printf("\nediting staff\n");
+
+    printf("%d %d\n\n", ROWS, COLUMNS);
+
+    for (int i=0; i<ROWS; ++i) {
+        for (int j=0; j<COLUMNS; ++j) {
+            printf("%d ", student_seats[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");   
+
+    for (int i=0; i<10; ++i) {
+        for (int j=0; j<10; ++j) {
             printf("%d ", heat_map[i][j]);
         }
         printf("\n");
